db3ea205a06577967c864d5508f453074ff9ca46
Adrianna Nowak <adriannanowakk@gmail.com>
2023-03-02 11:22:21 +0100

    Remove printMessages and put console.logs instead, add final options to win or lose the game

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-​
-console.log('Wylosowana liczba to: ' + randomNumber);
-​
-let computerMove = 'nieznany ruch';
-​
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if (randomNumber == 2) {
-  playerMove = "papier";
-} 
-else if (randomNumber == 3) {
-  playerMove = "nożyce";
+
+console.log("Wylosowana liczba to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else if (randomNumber == 3) {
+  computerMove = "nożyce";
 }
 
-
-​
-printMessage('Mój ruch to: ' + computerMove);
+console.log("Ruch komputera to: " + computerMove);
 
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
 
@@ -32,4 +28,21 @@ if (playerInput == "1") {
   playerMove = "nożyce";
 }
 
-printMessage("Twój ruch to: " + playerMove);
+console.log("Twój ruch to: " + playerMove);
+
+console.log(computerMove);
+console.log(playerMove);
+
+if (computerMove == "kamień" && playerMove == "papier") {
+  alert("Ty wygrywasz!");
+} else if (computerMove == "papier" && playerMove == "nożyce") {
+  alert("Ty wygrywasz!");
+} else if (computerMove == "nożyce" && playerMove == "kamień") {
+  alert("Ty wygrywasz!");
+} else if (computerMove == playerMove) {
+  alert("Remis!");
+} else if (playerMove == "nieznany ruch") {
+  alert("Wpisałeś liczbę inną niż 1, 2 lub - spróbuj ponownie.");
+} else {
+  alert("Komputer wygrywa!");
+}
