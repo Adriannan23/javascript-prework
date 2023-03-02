0c20d0b45d4caaa8b03d16fc31f5d4e564cf7401
Adrianna Nowak <adriannanowakk@gmail.com>
2023-03-02 10:44:13 +0100

    Add computer move logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+​
+console.log('Wylosowana liczba to: ' + randomNumber);
+​
+let computerMove = 'nieznany ruch';
+​
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if (randomNumber == 2) {
+  playerMove = "papier";
+} 
+else if (randomNumber == 3) {
+  playerMove = "nożyce";
+}
+
+
+​
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
 
 console.log("Gracz wpisał: " + playerInput);
